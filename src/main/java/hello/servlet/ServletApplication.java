ef439d1ee691e0c3cb1735f77961cd88a736abc2
gill <uint777@gmail.com>
2022-07-19 23:54:12 +0900

    test

--- src/main/java/hello/servlet/ServletApplication.java
+++ src/main/java/hello/servlet/ServletApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ServletApplication {
-
+// 테스트
 	public static void main(String[] args) {
 		SpringApplication.run(ServletApplication.class, args);
 	}
