8dc3f6d802f5dce776acb8f3e835f292054568fa
gill <uint777@gmail.com>
2022-08-05 22:16:24 +0900

    jsp추가

--- src/main/java/hello/servlet/web/servlet/MemberSaveServlet.java
+++ src/main/java/hello/servlet/web/servlet/MemberSaveServlet.java
@@ -1,5 +1,6 @@
 package hello.servlet.web.servlet;
 
+import hello.servlet.domain.member.Member;
 import hello.servlet.domain.member.MemberRepository;
 
 import javax.servlet.ServletException;
@@ -9,30 +10,39 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Enumeration;
 
-@WebServlet(name = "memberFormServlet", urlPatterns = "/servlet/members/new-form")
-public class MemberFormServlet extends HttpServlet {
+@WebServlet(name = "memberSaveServlet", urlPatterns = "/servlet/members/save")
+public class MemberSaveServlet extends HttpServlet {
 
     private MemberRepository memberRepository = MemberRepository.getInstance();
 
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("MemberSaveServlet.service");
+
+        String username = req.getParameter("username");
+        int age = Integer.parseInt(req.getParameter("age"));
+
+        Member member = new Member(username, age);
+        memberRepository.save(member);
+
         resp.setContentType("text/html");
         resp.setCharacterEncoding("utf-8");
         PrintWriter w = resp.getWriter();
-        w.write("<!DOCTYPE html>\n" +
-                "<html>\n" +
+        w.write("<html>\n" +
                 "<head>\n" +
                 " <meta charset=\"UTF-8\">\n" +
-                " <title>Title</title>\n" +
                 "</head>\n" +
                 "<body>\n" +
-                "<form action=\"/servlet/members/save\" method=\"post\">\n" +
-                " username: <input type=\"text\" name=\"username\" />\n" +
-                " age: <input type=\"text\" name=\"age\" />\n" +
-                " <button type=\"submit\">전송</button>\n" +
-                "</form>\n" +
+                "성공\n" +
+                "<ul>\n" +
+                " <li>id="+member.getId()+"</li>\n" +
+                " <li>username="+member.getUsername()+"</li>\n" +
+                " <li>age="+member.getAge()+"</li>\n" +
+                "</ul>\n" +
+                "<a href=\"/index.html\">메인</a>\n" +
                 "</body>\n" +
-                "</html>\n");
+                "</html>");
     }
 }
